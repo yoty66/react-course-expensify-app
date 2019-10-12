46262c0aee1f5c5487c23fc85c6c3d8f68fed876
Avraham <Yotam.Avraham@carlsonwagonlit.com>
2019-10-12 14:33:03 +0300

    changed format

--- src/app.js
+++ src/app.js
@@ -12,7 +12,7 @@ import moment from 'moment';
 const store =configureStore ();
 // console.log(store.getState());
 
-store.dispatch(addExpense({description: 'Slovenia Croatia' , createdAt: moment().valueOf() , amount: 1000000000000 , note:'מאמי מאמ את חייבת לי את החיים שלך'}));
+store.dispatch(addExpense({description: 'Slovenia Croatia' , createdAt: moment().valueOf() , amount: 100000000 , note:'מאמי מאמ את חייבת לי את החיים שלך'}));
 // store.dispatch(addExpense({description: 'REnt' , createdAt: 3 , amount: 3000000 }));
 
 
--- src/components/ExpenseListItem.js
+++ src/components/ExpenseListItem.js
@@ -1,11 +1,17 @@
 import React from 'react';
 import {NavLink} from "react-router-dom";
+import moment from 'moment';
+import numeral from 'numeral';
 
 
 const ExpenseListItem = ({id,description, amount , createdAt }) => (
     <div>
         <NavLink to={`/edit/${id}`} > <h3>{description}</h3></NavLink>
-        <p>{amount} -  {createdAt}</p>
+        <p>
+            {numeral(amount/100).format('$0,0.00').replace('$','₪')}
+            -
+            {moment(createdAt).format('MMMM Do, YYYY')}
+        </p>
 
     </div>
 );
